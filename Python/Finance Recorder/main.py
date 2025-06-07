51a8d60a7cee9f8c8b3ee118ec888ed638d456b1
WaffleLover <barontang77@gmail.com>
2025-06-07 03:16:13 +0100

    Added beginnings of record creation [NOT FINISHED] (Python)

--- Python/Finance Recorder/main.py
+++ Python/Finance Recorder/main.py
@@ -1,5 +1,12 @@
 import nicegui
 import os
+import csv
+
+headers = ["Date", "Record name", "Change in value"]
+
+test_entry = ["Test", "Test", "Test"]
+
+test_data = dict(zip(headers, test_entry))
 
 def get_folder():
     file_path = os.path.abspath(__file__)
@@ -10,7 +17,19 @@ def get_folder():
 
     return records_folder
 
+def create_new_record(name, records_folder):
+    record_path = os.path.join(records_folder, name) + ".csv"
+
+    if not os.path.isfile(record_path):
+        with open(record_path, "w", newline="") as record:
+            writer = csv.DictWriter(record, fieldnames=headers)
+            writer.writeheader()
+            writer.writerow(test_data)
+    else:
+        with open(record_path, "a", newline="") as record:
+            writer = csv.DictWriter(record, fieldnames=headers)
+            writer.writerow(test_data)
 
 
 if __name__ == "__main__":
-    print(get_folder())
+    create_new_record("Test", get_folder())
